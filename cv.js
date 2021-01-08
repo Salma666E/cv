14f37803045a26f373f3e5adcd8ec9d3e5a253f8
salma <salmaessam6666@yahoo.com>
2021-01-08 02:29:02 +0200

    first commit

---
+++ cv.js
@@ -0,0 +1,23 @@
+// 2- In assignment No.1, Use hidden audio element (without controls), that plays ringing sound when mouse is over contact section,
+//  and stop it when the mouse is away (You can search for a sound here: https://www.soundsnap.com/search/audio ).
+// 3- In Assignment No.1, use video element to display a video about you in your page.
+var sec=document.getElementById("Contant");
+var audio=document.getElementById("audio");
+sec.addEventListener("mouseover", function(  ) {   
+    audio.play();
+  }, false);
+sec.addEventListener("mouseout", function(  ) {   
+}, false);
+
+
+////////////canvas///////////
+/*Our Coding is divided into three parts:-
+1-Variable Declaration
+2-Creating a simple circle on Page.
+3-Creating progress circle on that circle.*/
+
+//1-Variable Declaration:
+
+//2-Creating a simple circle on Page:
+
+//3-Creating progress circle on that circle:
